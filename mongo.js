8b2531ff3cbbb3719ea9447e3bf6876374aa7404
Essi <essi.huhtanen@tuni.fi>
2023-07-06 21:44:41 +0300

    3.12 complete

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const newName = process.argv[3];
+const newNumber = process.argv[4];
+
+const url = `mongodb+srv://databaseuser:${password}@cluster0.rniiz7m.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: newName,
+  number: newNumber,
+});
+
+if (newName && newNumber) {
+  person.save().then((result) => {
+    console.log(`Added ${newName} number ${newNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
